62ac2d69d86f539b1fe12ccb62e66aab23d6d2ef
Herick Vinicius <herick.gustavo@gmail.com>
2020-11-14 23:26:41 -0400

    Adicionando estudos sobre API com Express.

---
+++ node/projeto/src/servidor.js
@@ -0,0 +1,44 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true })) //body parser é um middleware que transforma os dados do post em objetos.
+
+app.get('/produtos', (req, res, next) => {
+    // res.send já converte o objeto para JSON.
+    //res.send({ nome: 'Notebook', preco: 123.45 })
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,    //nome
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,    //nome
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) //JSON
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor executando na porta ${porta}.`)
+})
